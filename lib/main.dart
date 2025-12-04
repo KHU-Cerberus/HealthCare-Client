8614a62b3b77d6b2f4d0d475c6c5ebcb2430b552
King_Eon <0302tjddjs@gmail.com>
2025-12-04 16:15:52 +0900

    feat: android permission, improve login/register UI

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:health_care/presentation/login/login.dart';
 import 'core/camera/camera_config.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   cameras = await availableCameras(); // 사용 가능한 카메라 목록 가져오기
-
+  await dotenv.load(fileName: '.env');
+  String baseUrl = dotenv.env['SERVER_URL'] ?? "";
   runApp(MyApp(
-    baseUrl: 'http://10.0.2.2:8080',
+    baseUrl: baseUrl,
   ));
+
 }
 
 class MyApp extends StatelessWidget {
@@ -18,7 +21,7 @@ class MyApp extends StatelessWidget {
   final String baseUrl;
 
   const MyApp({super.key, required this.baseUrl});
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/presentation/style/colors.dart
+++ lib/presentation/style/colors.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 const Color blackColor = Color(0xFF121212);
 const Color whiteColor = Color(0xFFFAFAFA);
+const Color grayColor = Color(0xFFD4D4D4);
+const Color darkGrayColor = Color(0xFF6C6C6C);
 const Color deepBlueColor = Color(0xFF0084FF);
 const Color blueColor = Color(0xFF5BC8FF);
 const Color lightBlueColor = Color(0xFFD7F1FF);
